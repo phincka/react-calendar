aadb4f76a1b84b86cc5a5c5d2440a84fca973f77
Paweł Hincka <pawel@hincka.pl>
2020-01-20 20:33:48 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Header from './components/header'
+
+import './css/main.scss';
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <Header />
+            </div>
+        )
+    }
+}
+
+export default App;
